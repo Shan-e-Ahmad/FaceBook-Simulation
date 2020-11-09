a0dfecab3f5c30b54ce0d74f4dbfef6f97bfad40
Hoc Vultus Paic <74147902+hovacp@users.noreply.github.com>
2020-11-09 08:08:41 +0500

    Add files via upload

--- Text/read.java
+++ Text/read.java
@@ -6,33 +6,34 @@ import java.util.ArrayList;
 import java.io.FileNotFoundException;
 public class read{
 
-public void reading(String str){
+    public void reading(String str){
 
-try{
+        try{
 
 
-File obj1=new File(str);
-Scanner obj2=new Scanner(obj1);
+            File obj1=new File(str);
+            Scanner obj2=new Scanner(obj1);
 
-if (!obj1.exists())
-{
-obj1.createNewFile();
-}
+            if (!obj1.exists())
+            {
+                //obj1.createNewFile();
+            }
+            //to read word by word in a line use 'hasNext()' function instead of 'hasNextLine()'
+            while(obj2.hasNextLine())
+            {
+                String data= obj2.nextLine();
+                System.out.print(data);
+                System.out.println("\n");
+            }
+            obj2.close();
 
-while(obj2.hasNextLine())
-{
-String data= obj2.nextLine();
-System.out.print(data);
-}
-obj2.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.print("Error cannot open File");
+            e.printStackTrace();
+        }
 
-}
-catch(FileNotFoundException e)
-{
-System.out.print("Error cannot open File");
-e.printStackTrace();
-}
-
-}
+    }
 
 }
