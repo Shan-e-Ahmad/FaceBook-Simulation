3f66545a57a9ff517d31f473f88929f4fa5a6cef
Shan-e-Ahmad <74091276+Shan-e-Ahmad@users.noreply.github.com>
2020-11-15 04:24:01 +0500

    Add files via upload

--- DB/DB layer.sql
+++ DB/DB layer.sql
@@ -1,53 +1,276 @@
-create database facebook
+drop database Facebook
+create database Facebook
+use Facebook
 
-use facebook
 create table [user](
-id int not null primary key,
+userid int not null primary key,
 name varchar(100),
 email varchar(100),
 [password] varchar(100),
 dateofbirth date,
-gender varchar(100)
+gender nvarchar(100),
+)
+
+create table friends(
+    userid int ,
+    FriendID int,
+	foreign key(userid) references [user](userid),
+	foreign key(FriendID) references [user](userid),
+	CONSTRAINT pk_friends PRIMARY KEY (userid,FriendID)
+)
+
+Create Table Post
+(
+postid int NOT NULL PRIMARY KEY,
+userid int FOREIGN KEY REFERENCES [user](userid),
+text nvarchar(1000)
+)
+
+create Table Comment
+(
+Commentid int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+userid int FOREIGN KEY REFERENCES [user](userid),
+postid int FOREIGN KEY REFERENCES post(postid),
+text nvarchar(500)
+)
+
+Create Table Likepost
+(
+userid int FOREIGN KEY REFERENCES [user](userid),
+postid int FOREIGN KEY REFERENCES post(postid),
+CONSTRAINT pk_Likedpost PRIMARY KEY (userid,postid)
 
 )
-create table friendsname(
-id int ,
-    FriendID int,
-	foreign key(id) references [user](id),
-	foreign key(FriendID) references [user](id)
+
+create Table Share
+(
+shareid int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+userid int FOREIGN KEY REFERENCES [user](userid),
+postid int FOREIGN KEY REFERENCES post(postid)
 )
-select * from[user]
-select * from friendsname
-insert into [user] values(1,'Shan e ahmad','shan e ahmad@gmail.com','123456789','1999-05-12','M')
-insert into [user] values(2,' ahmad',' ahmad@gmail.com','23456789','1999-08-25','M')
-insert into [user] values(3,'mozaina','mozaina@gmail.com','456789','2000-06-2','F')
-insert into friendsname values(1,2)
-insert into friendsname values(1,3)
-insert into friendsname values(2,3)
-drop table dbo.post
-create table post(
-postid int identity(1,1)not null primary key ,
-userid int  foreign key references [user](id),
-text varchar(5000)
+
+
+create table Video(
+Videoid int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+trailer_link varchar(200),
 )
-create procedure createpost
-@id int ,
-@text varchar(5000),
-@output int output
-as  
-begin
-if exists(select * from [user] 
-where [user].id=@id)
-begin
-insert into post(userid,text)values(@id,@text)
-set @output=1
+
+create Table Message(
+messageid int IDENTITY(1,1) not null PRIMARY KEY,
+senderid int FOREIGN KEY REFERENCES [user](userid),
+recvrid int FOREIGN KEY REFERENCES [user](userid),
+message nvarchar(1000)
+)
+
+Create Table Page
+(
+Pageid int NOT NULL PRIMARY KEY,
+adminid int FOREIGN KEY REFERENCES [user](userid),
+name varchar(100),
+description nvarchar(100)
+)
+
+Create Table FGroup
+(
+Groupid int NOT NULL PRIMARY KEY,
+adminid int FOREIGN KEY REFERENCES [user](userid),
+name varchar(100),
+description nvarchar(100)
+)
+
+
+
+go
+
+create Procedure Usersignup 
+@userid int,
+@name varchar(100),
+@email varchar(100),
+@password varchar(100),
+@dateofbirth date,
+@gender char,
+@output int OUTPUT
+As
+Begin
+
+if exists (select * From [user] where [user].userid=@userid)
+Begin
+set @output= -2
+
+return
+End
+else if exists (select * From [user] where [user].email=@email)
+Begin
+set @output = 0
+return
+End
+else
+Insert into [user] values (@userid,@name, @email, @password,@dateofbirth,@gender )
+set @output= 1
+End
+
+go
+
+declare  @out int
+exec Usersignup 1, 'shan', 'doublecheck1@gmail.com', 'pass', '2000-01-17', 'M', @out OUT
+Select @out
+go
+
+
+-- UserLogin
+  go
+create Procedure UserLogin  
+@email varchar(100), 
+@password varchar(100), 
+@output int OUTPUT
+ As
+ Begin
+
+  if  not exists (select * From [user] where [user].email=@email AND [user].[password] =@password)
+  Begin
+  set @output = 0
+  return
+  End
+  set @output = 1
+ End
+ go
+
+ go
+ declare  @out int
+ exec UserLogin 'doublecheck1@gmail.com', 'pass', @out  out
+ Select @out
+ go
+
+
+ 
+
+-- Video
+  go
+  drop procedure Videos
+create Procedure Videos  
+
+@trailer_link varchar(200), 
+@output int OUTPUT
+ As
+ Begin
+  
+ 
+  
+ Insert into Video values (@trailer_link)
+ set @output= 1
+End
+ go
+
+ go
+ declare  @out int
+ exec Videos'https://www.youtube.com/embed/-Denciie5oA', @out  output
+ Select @out
+ go
+
+
+
+ create Procedure createPage 
+@Pageid int,
+@adminid int,
+@name varchar(100),
+@description nvarchar(100),
+@output int OUTPUT
+As
+Begin
+
+if exists (select * From Page where Page.Pageid=@Pageid)
+Begin
+set @output= -2
+return
+End
+
+else if exists (select * From Page where Page.name=@name)
+Begin
+set @output = 0
+return
+End
+else
+Insert into Page values (@Pageid,@adminid,@name, @description)
+set @output= 1
+End
+
+go
+
+declare  @out int
+exec createPage 1,1, 'page1', 'first page ever', @out OUT
+Select @out
+go
+
+
+create Procedure viewGroup
+@groupid int
+As
+Begin
+select *
+from FGroup 
+where FGroup.Groupid=@groupid
+End
+go
+
+exec viewGroup 1
+go
+
+
+
+create Procedure createpost 
+@postid int ,
+@userid int,
+@text nvarchar(1000),
+@output int OUTPUT
+As
+Begin
+
+if exists (select * From Post where Post.postid=@postid)
+Begin
+set @output= -2
+return
+End
+else
+Insert into Post values (@postid, @userid,@text )
+set @output= 1
+End
+
+go
+
+declare  @out int
+exec createpost 1,1, 'Post1', @out OUT
+Select @out
+go
+
+
+
+create Procedure SendMessage
+@senderid int,
+@recvrid int,
+@message nvarchar(1000),
+@output int OUTPUT
+As
+Begin
+
+if exists (select * From [user] where [user].userid=@senderid)
+Begin
+if exists (select * From [user] where [user].userid=@recvrid )
+Begin
+if exists (select * From [user] where @senderid!=@recvrid )
+Begin
+Insert into Message values (@senderid, @recvrid, @message)
+set @output= 1 --Message added successfully
 end
 else
-set @output =-1
-return
+set @output= -4 --User cannot send a message to itself
 end
-declare @output1 int 
-exec createpost 1,'my name is shaaaaaaaaan ',@output1 out
-select @output1
-select * from post
-drop procedure createpost
+else 
+set @output= -3 --reciever does not exists
+return
+End
+else
+set @output= -2 --sender does not exists
+return
+End
+
+go
