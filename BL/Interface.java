dae5790055329e15606d74db9ae86e0ad0d56c4d
Muhammad Sheharyar Mohsin <74047957+msm-823@users.noreply.github.com>
2020-11-16 01:22:13 +0500

    Add files via upload

---
+++ BL/Interface.java
@@ -0,0 +1,16 @@
+package Test2;
+
+interface Interfaces {
+
+
+        public void updateStatus();
+        public boolean equals(Object o);
+        public int hashCode();
+        public String toString();
+        public void print();
+        public void message(User u,String Message);
+        public void LikePosts(Posts pid, User uid);
+        public void share(Posts pid, User uid);
+        void GroupView(String GroupName);
+
+        }
