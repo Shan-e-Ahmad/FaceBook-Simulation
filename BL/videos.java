b6191e63a24a60958bee98ec291a27cd423dc169
Muhammad Sheharyar Mohsin <74047957+msm-823@users.noreply.github.com>
2020-11-15 23:59:16 +0500

    Add files via upload

---
+++ BL/videos.java
@@ -0,0 +1,13 @@
+package Test2;
+
+public class videos implements interfaces{
+
+
+    void play_videos(String Vidname)
+    {
+
+        System.out.println("Click Play to Play Video: ");
+        System.out.println("Video" +Vidname +"is Playing ");
+    }
+
+}
